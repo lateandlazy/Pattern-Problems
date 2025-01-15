c31d91f6a6112cfe05a2f3d4fd8c82167804baa9
lateandlazy <rehan7050355196@gmail.com>
2025-01-15 19:04:38 +0530

    Add files via upload

---
+++ Question4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{   
+int n , i ,j ;
+n = 5;
+for (i=0;i<n;i++)
+{
+    for (j=0;j<n;j++)
+    {
+        if (i>0 && i<n-1 && j>0 && j<n-1)
+        {
+            printf(" ");
+        }
+        else
+        {
+            printf("%d",j+1);
+        }
+    }
+    printf("\n");
+}
+
+}
